0759923cf4ff816b4bcceb87a0238cfbed6974f2
AlirezaAkbarpour <akbarpouralireza1>
2024-01-26 14:10:24 +0330

    fix things

--- components/actions/feedback.tsx
+++ components/actions/feedback.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 export default function Feedback() {
-  const message = React.useRef<HTMLInputElement|undefined>(null)
-  const messageHandler = (e:any)=>{
-    const text = message.current?.value
+  const message : React.RefObject<HTMLInputElement|null> = React.useRef(null)
+  const messageHandler = (e:HTMLFormElement)=>{
+    const text:string = message.current?.value
     if(text != ''){
       alert("your message send!")
       message.current.value = ''
@@ -22,8 +22,8 @@ export default function Feedback() {
 
   return (
     <div className='w-3/4 h-16 p-2 mt-6 rounded-xl bg-violet-800 '>
-            <form className='flex' onSubmit={messageHandler}>
-                <input type='text' className='font-mono font-normal w-2/3 p-2 m-1' placeholder='Enter your message:' ref={message} onChange={(e)=>changeHandler(e)}></input>
+            <form className='flex' onSubmit={(e:any)=>messageHandler(e)}>
+                <input type='text' className='font-mono font-normal w-2/3 p-2 m-1' placeholder='Enter your message:' ref={message} onChange={(e:any)=>changeHandler(e)}></input>
                 <button className='w-1/3 m-1 p-2 bg-white text-violet-950 hover:bg-violet-400 hover:transition-all hover:delay-100 hover:text-white' type='submit' onClick={(e)=>messageHandler(e)}>Send Notification</button>
             </form>
     </div>
