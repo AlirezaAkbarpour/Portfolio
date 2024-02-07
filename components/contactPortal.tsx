12cdd6f9680a6aa81a78709cdbb16cb729707e72
AlirezaAkbarpour <akbarpouralireza1>
2024-02-07 22:44:20 +0330

    contact portal finish

--- components/contactPortal.tsx
+++ components/contactPortal.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import CancelIcon from '@mui/icons-material/Cancel';
-import IconButton from '@mui/material';
+import InstagramIcon from '@mui/icons-material/Instagram';
 import { createPortal } from 'react-dom';
 import { Divider } from '@mui/material';
+import Link from 'next/link';
+import { Email, Opacity, Telegram } from '@mui/icons-material';
+import { gsap } from 'gsap/gsap-core';
+import { useGSAP } from '@gsap/react';
+
 type Props = {
     isOpen:boolean,
     OnClose:any
@@ -10,19 +15,58 @@ type Props = {
 
 
 export default function ContactPortal({isOpen,OnClose}:Props){
+    
+    const layout : React.RefObject<HTMLDivElement> = useRef(null)
+    const timeline = gsap.timeline()
+    const {contextSafe} = useGSAP({scope:layout})
+    const closeHandler = ()=>{
+        const Onclicked = contextSafe(()=>{
+            timeline.to(layout.current,{opacity:0,y:40,ease:'power1.out'})
+        })
+        Onclicked();
+        setTimeout(()=>{        
+            OnClose(false);
+        },400)
+    }
+
+    useGSAP(()=>{
+       isOpen?timeline.to(layout.current,{opacity:1,y:-40}):null
+    },{scope:layout})
+    
     if(!isOpen) return null;
 
     return createPortal(
-        <div className='w-[640px] h-[420px] bg-white rounded-lg fixed top-1/4 left-1/3 shadow-2xl'>
+        <div className='w-[640px] h-[360px] bg-white opacity-0 rounded-lg p-2 fixed top-1/4 left-1/3 shadow-2xl' ref={layout}>
                 <div className='w-full h-16 flex justify-between p-4'>
                         <h1 className='text-4xl font-archivo font-semibold text-footer-color-black'>Contact Me</h1>
-                        <button onClick={()=>OnClose(false)}>
-                        <CancelIcon className='w-16 h-16 cursor-pointer text-red-800 mt-1 hover:opacity-50' fontSize='large' 
+                        <button onClick={closeHandler}>
+                        <CancelIcon className='w-16 h-16 cursor-pointer text-orange-600 mt-1 hover:opacity-50' fontSize='large' 
                         />
                         </button>
                       
                 </div>  
-                <Divider color='grey'/>
+                <Divider color='grey' variant='middle'/>
+                <div className='flex justify-center'>
+
+                <div className="flex justify-center w-1/3 p-4 mt-8 hover:border-2 hover:border-purple-900 hover:scale-105 rounded-lg delay-75">
+                    <Link href={'/'} >
+                        <InstagramIcon sx={{fontSize:"108px"}} className='text-orange-600 ml-2 text-center'/>
+                        <h1 className='text-center font-semibold text-footer-color-black text-2xl p-2 mt-8'>Instagram</h1>
+                    </Link>
+                </div>
+                <div className="flex justify-center w-1/3 p-4 mt-8 hover:border-2 hover:border-purple-900 hover:scale-105 rounded-lg delay-75">
+                    <Link href={'https://t.me/09945978023'} >
+                        <Telegram sx={{fontSize:"108px"}} className='text-orange-600 ml-2 text-center'/>
+                        <h1 className='text-center font-semibold text-footer-color-black text-2xl p-2 mt-8'>Telegram</h1>
+                    </Link>
+                </div>
+                <div className="flex justify-center w-1/3 p-4 mt-8 hover:border-2 hover:border-purple-900 hover:scale-105 rounded-lg delay-75">
+                    <Link href={'https://akbarpouralireza1@gmail.com'} type='email'>
+                        <Email sx={{fontSize:"108px"}} className='text-orange-600 ml-2 text-center'/>
+                        <h1 className='text-center font-semibold text-footer-color-black text-2xl p-2 mt-8'>Email</h1>
+                    </Link>
+                </div>
+                </div>
         </div>
     ,document.getElementById("not") as HTMLElement)
 }
