314f71fea5756d5cf7c59fe2b99fb909a27f3f38
AlirezaAkbarpour <akbarpouralireza1>
2024-10-25 11:24:07 +0330

    working on add project modal

--- components/projectForm.tsx
+++ components/projectForm.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useEffect, useState } from "react"
 import { createPortal } from "react-dom"
+import Add_Icon from "./svgs/add_Icon"
 
 export default function ProjectForm() {
   const [isClient,setClient] = useState(false)
@@ -24,16 +25,49 @@ export default function ProjectForm() {
                 <div className="w-4 h-4 rounded-full bg-blue-500 mx-1"></div>
               </div>
                 <h1 className="text-2xl h-10 font-medium text-zinc-600 mx-8">Setting Project</h1>
-              <div className="w-full h-5/6 overflow-y-scroll">
+              <div className="w-full h-5/6 overflow-y-scroll flex justify-between">
                 <div className="w-1/2 mx-8 pr-24 ">
                   <h2 className="text-xl font-normal text-zinc-600 mt-6 mx-2 flex items-center">id: <p className="text-zinc-400 px-2 text-xl">22a5255</p></h2>
                   <h2 className="text-xl font-medium text-zinc-600 mt-6 mx-2">Name Project:</h2>
                   <input type="text" placeholder="name" className="w-full h-10 ml-6 my-4 px-2 text-lg font-light ring-1 ring-zinc-400 rounded-md"/>
                   <h2 className="text-xl font-medium text-zinc-600 mt-4 mx-2">Description Project:</h2>
-                  <textarea  placeholder="name" className="w-full min-h-40 max-h-80 overflow-y-hidden  ml-6 my-4 px-2 text-lg font-light ring-1 ring-zinc-400 rounded-md"/>
-                  <h2 className="text-xl font-medium mx-2 mt-4 text-zinc-600">Feedback Options</h2>
+                  <textarea placeholder="information about project" className="w-full min-h-40 max-h-80 overflow-y-hidden  ml-6 my-4 py-2 px-2 text-lg font-light ring-1 ring-zinc-400 rounded-md"/>
+                  <div className="w-full">
+                    <h2 className="text-xl font-medium mx-2 mt-4 text-zinc-600">Feedback Options</h2>
+                    <div className="w-full flex justify-start mx-6 my-2">
+                      <div className="w-20 flex justify-between items-center">
+                        <label htmlFor="#likes" className="text-xl text-zinc-600">Likes:</label>
+                        <input type="checkbox" className="w-4 h-4 "/>
+                      </div>
+                      <div className="w-20 flex justify-between items-center ml-6">
+                        <label htmlFor="#links" className="text-xl text-zinc-600">Links:</label>
+                        <input type="checkbox" className="w-4 h-4 "/>
+                      </div>
+                      <div className=" flex justify-between items-center ml-6">
+                        <label htmlFor="#comments" className="text-xl text-zinc-600">Comments:</label>
+                        <input type="checkbox" className="w-4 h-4 mx-2"/>
+                      </div>
+                    </div>
+                  </div>
                 <div>
               </div>
+                </div>
+                <div className="w-1/2 mx-8">
+                  <h1 className="text-xl text-zinc-600 font-medium mt-6">Add Links</h1>
+                  <div className="flex mt-6 ml-4">
+                    <h1 className="w-28 text-lg font-normal text-zinc-600">Demo Site:</h1>
+                    <input type="text" placeholder="https://simple.com" className="w-80 h-8 mx-4 font-normal rounded-md ring-1 p-1 ring-zinc-400 text-zinc-600"/>
+                  </div>
+                  <div className="flex mt-6 ml-4">
+                    <h1 className="w-28 text-start text-lg font-normal text-zinc-600">Github Page:</h1>
+                    <input type="text" placeholder="https://simple.com" className="w-80 font-normal h-8 mx-4 rounded-md ring-1 p-1 ring-zinc-400 text-zinc-600"/>
+                  </div>
+                  <div className="w-full flex justify-center">
+                    <div className="w-full text-lg ml-2 mr-8 px-4 rounded-md my-8 bg-dashboard-orange flex justify-center">
+                      <h1 className="text-white flex justify-center items-center">Add Link Addition <Add_Icon/> </h1>
+                    </div>
+                  </div>
+                  
                 </div>
               </div>
             </div>
---
+++ components/svgs/add_Icon.tsx
@@ -0,0 +1,10 @@
+export default function Add_Icon() {
+  return (
+    <div className="m-2">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect x="1" y="1" width="22" height="22" rx="9" stroke="white" stroke-width="2" stroke-miterlimit="4.13357" stroke-dasharray="5 5"/>
+        <path d="M11.1412 17V12.76H7.28117V11.04H11.1412V6.78H12.8412V11.04H16.7212V12.76H12.8412V17H11.1412Z" fill="white"/>
+        </svg>
+    </div>
+  )
+}
