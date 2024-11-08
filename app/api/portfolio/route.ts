2c3420fee9f9c1e98c02d2be9931c8dacfbc5309
AlirezaAkbarpour <akbarpouralireza1>
2024-11-08 13:03:12 +0330

    push to github v2.0

--- app/api/portfolio/route.ts
+++ app/api/portfolio/route.ts
@@ -1,13 +1,34 @@
 import dbConnect from "@/lib/db";
-import { Portfolio } from "@/types/portfolios";
-import { NextApiResponse ,NextApiRequest } from "next";
+//import { Portfolio } from "@/types/portfolios";
+import Portfolio from '@/models/portfolio';
 
-
-export async function handler(req:NextApiRequest,res:NextApiResponse) {
-    const {method} = req;
+export async function GET(req:Request) {
     await dbConnect;
-    switch(method){
-        case "GET":
-
+    try{
+        const portfolios = await Portfolio.find({});
+        return Response.json({success:true,data:portfolios},{status:200})
+    } catch(error){
+        return Response.json({success:false,error:error},{status:400})
     }
 };
+
+const data = { id:256245,
+    name:"Project One",
+    description:"Project code",
+    likes:25,
+    links:{
+        github:"https://github.com",
+        main_page:"page"
+    }}
+
+export async function POST(req:Request) {
+    await dbConnect;
+    try{
+        const newPortfolio = await Portfolio.create(
+            JSON.stringify(data)
+        );
+        return Response.json({success:true,data:newPortfolio},{status:201,headers:{"Content-Type":"application/json"}})
+    }catch(error){
+        return Response.json({success:false},{status:400})
+    }
+}
--- lib/db.ts
+++ lib/db.ts
@@ -35,7 +35,6 @@ async function dbConnect(){
         cached.promise = null
         throw e;
     }
-
     return cached.conn
 }
 
